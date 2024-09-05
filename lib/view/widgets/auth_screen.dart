94cde26a0d6ba2001f993742cd2745443a25f9da
Rashid1110 <rashidrashi000432@gmail.com>
2024-09-05 13:43:48 +0530

    verified

--- lib/view/widgets/auth_screen.dart
+++ lib/view/widgets/auth_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:food/controllers/providers/providers.dart';
 import 'package:food/view/pages/products_screen.dart';
-import 'package:food/view/widgets/phone_screen.dart';
+import 'package:food/view/widgets/phone_auth_screen.dart';
 
 class AuthScreen extends ConsumerWidget {
   const AuthScreen({super.key});
@@ -14,24 +14,87 @@ class AuthScreen extends ConsumerWidget {
 
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Google Sign-In'),
+          title: const Text('Calories Products'),
+          centerTitle: true,
+          elevation: 5,
         ),
         body: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            ElevatedButton(
-              onPressed: () async {
-                await authNotifier.signInWithGoogle();
-                Navigator.pushReplacement(
-                    context, MaterialPageRoute(builder: (_) => ProductsPage()));
-              },
-              child: const Text('Sign In with Google'),
+          child: Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 35, right: 35, bottom: 25),
+              child: ElevatedButton(
+                onPressed: () async {
+                  await authNotifier.signInWithGoogle();
+                  // ignore: use_build_context_synchronously
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (_) => const ProductsPage()));
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue[800],
+                  padding: const EdgeInsets.symmetric(vertical: 15.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * .10,
+                    ),
+                    CircleAvatar(
+                      radius: 13,
+                      backgroundColor: Colors.white,
+                      child: Image.network(
+                        'https://altogethergroup.com.au/static/img/google-g-logo.png',
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * .20,
+                    ),
+                    const Text(
+                      'Google',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
             ),
-            ElevatedButton(
-              onPressed: () async {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (_) => PhoneAuthPage()));
-              },
-              child: const Text('Mobile Number'),
+            Padding(
+              padding: const EdgeInsets.only(left: 35, right: 35, bottom: 25),
+              child: ElevatedButton(
+                onPressed: () async {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (_) => PhoneAuthScreen()));
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green[800],
+                  padding: const EdgeInsets.symmetric(vertical: 15.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * .10,
+                    ),
+                    const Icon(
+                      Icons.phone,
+                      color: Colors.white,
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * .20,
+                    ),
+                    const Text(
+                      'Phone',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ]),
         ));
